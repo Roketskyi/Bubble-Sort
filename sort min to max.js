09ab4c837c8044a93790ada5ad2afd62336095a4
Roketskyi Roman <91413976+Roketskyi@users.noreply.github.com>
2022-11-01 13:22:15 +0200

    Add files via upload

---
+++ sort min to max.js
@@ -0,0 +1,16 @@
+const array = [63, 2, 15, 8, 3, -56, 1, -2];
+
+function bubbleSort(array){
+  for (let j = 0; j < array.length; j++){
+    for (let i = 0; i < array.length - 1; i++){
+      if (array[i] > array[i + 1]){
+        const result = array[i]
+        array[i] = array[i + 1]
+        array[i + 1] = result
+      }
+    }
+  }
+  console.log(array)
+}
+
+bubbleSort(array);
